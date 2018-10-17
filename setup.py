3149d012841c67b1f1c9019c9d4428e98357eeaf
Brandon Istenes <bistenes@gmail.com>
2018-10-16 21:56:24 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="ezcsv",
+    version="0.1",
+    description="Functions to do obvious things with CSVs",
+    author="Brandon Istenes",
+    author_email="brandonesbox@gmail.com",
+    url="",
+    packages=["ezcsv"],
+)
